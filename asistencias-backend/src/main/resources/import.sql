f36eb38952162dc4759a7375458a0c75090216a1
erikskate <erikjoel35@hotmail.com>
2022-10-31 12:42:28 -0600

    Se modifica el archivo import.sql

--- asistencias-backend/src/main/resources/import.sql
+++ asistencias-backend/src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO aula (nombre) VALUES ('1ERO', 'B');
+INSERT INTO aula (nombre) VALUES ('1ERO B');
 
-INSERT INTO estudiantes (nombre,apellidos,codigo, aula_id) VALUES ('Erik', 'Peimbert', '20202020',1);
-INSERT INTO estudiantes (nombre,apellidos,codigo, aula_id) VALUES ('Fernanda', 'Corona', '010010',1);
-INSERT INTO estudiantes (nombre,apellidos,codigo, aula_id) VALUES ('Cristian', 'Tellez', '30303030',1);
-INSERT INTO estudiantes (nombre,apellidos,codigo, aula_id) VALUES ('Mojik', 'Mojik', '4040404',1);
-INSERT INTO estudiantes (nombre,apellidos,codigo, aula_id) VALUES ('Victor', 'Sanchez', '5050505',1);
-INSERT INTO estudiantes (nombre,apellidos,codigo, aula_id) VALUES ('Michelle', 'Cabrera', '60606060',1);
-INSERT INTO estudiantes (nombre,apellidos,codigo, aula_id) VALUES ('Silvia', 'Cabrera', '70770070',1);
+INSERT INTO estudiante (nombre,apellidos,codigo, aula_id) VALUES ('Erik', 'Peimbert', '20202020',1);
+INSERT INTO estudiante (nombre,apellidos,codigo, aula_id) VALUES ('Fernanda', 'Corona', '010010',1);
+INSERT INTO estudiante (nombre,apellidos,codigo, aula_id) VALUES ('Cristian', 'Tellez', '30303030',1);
+INSERT INTO estudiante (nombre,apellidos,codigo, aula_id) VALUES ('Mojik', 'Mojik', '4040404',1);
+INSERT INTO estudiante (nombre,apellidos,codigo, aula_id) VALUES ('Victor', 'Sanchez', '5050505',1);
+INSERT INTO estudiante (nombre,apellidos,codigo, aula_id) VALUES ('Michelle', 'Cabrera', '60606060',1);
+INSERT INTO estudiante (nombre,apellidos,codigo, aula_id) VALUES ('Silvia', 'Cabrera', '70770070',1);
