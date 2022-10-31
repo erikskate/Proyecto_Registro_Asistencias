604fa8ece1bfb93125251d88f3647e48622a2ba0
erikskate <erikjoel35@hotmail.com>
2022-10-31 12:00:47 -0600

    Se agrega el proyecto backend

---
+++ asistencias-backend/src/main/java/com/asistenciasbackend/AsistenciasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.asistenciasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistenciasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsistenciasBackendApplication.class, args);
+	}
+
+}
