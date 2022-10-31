5797d4560d745e5657464b82d324b061f3d49e93
erikskate <erikjoel35@hotmail.com>
2022-10-31 14:54:29 -0600

    Se agregan los componentes asistencia,salida y reporte así como sus respectivas rutas y se agrega angular material al proyecto

--- asistencias-frontend/src/app/app.component.ts
+++ asistencias-frontend/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asistencias-frontend';
+
+  rutas: any[] = [
+    {
+      name: 'Registrar asistencia',
+      router: ['registro-asistencia']
+    },
+    {
+      name: 'Registrar salida',
+      router: ['registro-salida']
+    },
+    {
+      name: 'Reposrte de salida',
+      router: ['reporte']
+    }
+  ];
 }
--- asistencias-frontend/src/app/app.module.ts
+++ asistencias-frontend/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { RegistrarAsistenciaComponent } from './registrar-asistencia/registrar-asistencia.component';
+import { RegistrarSalidaComponent } from './registrar-salida/registrar-salida.component';
+import { ReporteComponent } from './reporte/reporte.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrarAsistenciaComponent,
+    RegistrarSalidaComponent,
+    ReporteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
